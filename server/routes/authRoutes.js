3369c666e4cbadfce6e367d27268f90425e404f6
xequinox08x-cmd <xequinox08x@gmail.com>
2026-02-27 15:20:35 +0800

    Delete server directory

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { register, login } = require("../controllers/authController");
-
-router.post("/register", register);
-router.post("/login", login);
-router.get("/test", (req, res) => res.json({ ok: true }));
-
-module.exports = router;
